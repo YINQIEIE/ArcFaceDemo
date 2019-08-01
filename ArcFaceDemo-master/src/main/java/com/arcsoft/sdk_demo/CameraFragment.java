36bfece46c564be44aa16e0dc24784db442eef37
yinqi <251282197.com>
2019-08-01 09:58:00 +0800

    修改旋转角度计算方法，适配横竖屏

--- ArcFaceDemo-master/src/main/java/com/arcsoft/sdk_demo/CameraFragment.java
+++ ArcFaceDemo-master/src/main/java/com/arcsoft/sdk_demo/CameraFragment.java
@@ -1,6 +1,7 @@
 package com.arcsoft.sdk_demo;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.ImageFormat;
 import android.graphics.Rect;
@@ -26,7 +27,6 @@ import com.guo.android_extend.widget.CameraSurfaceView;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class CameraFragment extends Fragment implements View.OnTouchListener, CameraSurfaceView.OnCameraListener, Camera.AutoFocusCallback, ViewTreeObserver.OnGlobalLayoutListener {
 
     private final String TAG = getClass().getSimpleName();
@@ -39,8 +39,9 @@ public class CameraFragment extends Fragment implements View.OnTouchListener, Ca
     private boolean mCameraMirror;
     private CameraSurfaceView surfaceView;
     private MyCameraGLSurfaceView glSurfaceView;
-    private int[] surfaceSize;
     private View rootView;
+    private int[] surfaceSize;
+    private int additionalRotation = 0;//额外旋转角度，特殊情况如一些定制设备，硬件安装不规范的时候使用
 
     private FRManager frManager;
 
@@ -76,9 +77,33 @@ public class CameraFragment extends Fragment implements View.OnTouchListener, Ca
      */
     private void initCameraParams() {
         mCameraID = mCameraID == 0 ? Camera.CameraInfo.CAMERA_FACING_BACK : Camera.CameraInfo.CAMERA_FACING_FRONT;
-        mCameraRotate = mCameraID == 0 ? 90 : 270;
+//        mCameraRotate = mCameraID == 0 ? 90 : 270;
+        mCameraRotate = getCameraOri(getActivity().getWindowManager().getDefaultDisplay().getRotation());
 //        mCameraMirror = cameraId == 0;//由于一体机后置摄像头朝前所以要加镜像
         mCameraMirror = mCameraID != 0;//平板等其他设备正常
+    }
+
+    /**
+     * 获取预览应旋转的角度
+     *
+     * @param rotation 屏幕旋转信息
+     * @return 角度
+     */
+    private int getCameraOri(int rotation) {
+        int degrees = rotation * 90;
+        additionalRotation /= 90;
+        additionalRotation *= 90;
+        degrees += additionalRotation;
+        int result;
+        Camera.CameraInfo info = new Camera.CameraInfo();
+        Camera.getCameraInfo(mCameraID, info);
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+//            result = (360 - result) % 360;
+        } else {
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        return result;
     }
 
     @Nullable
@@ -115,8 +140,13 @@ public class CameraFragment extends Fragment implements View.OnTouchListener, Ca
      * @return 宽高对调后的宽高数组
      */
     private int[] getSurfaceSize() {
-        View rlCamera = rootView.findViewById(R.id.glSurfaceView);
-        return new int[]{rlCamera.getMeasuredHeight(), rlCamera.getMeasuredWidth()};
+        View view = rootView.findViewById(R.id.glSurfaceView);
+        int orientation = getResources().getConfiguration().orientation;
+//        Log.i(TAG, "getSurfaceSize: width = " + view.getMeasuredWidth() + " height = " + view.getMeasuredHeight());
+        if (orientation == Configuration.ORIENTATION_PORTRAIT)
+            return new int[]{view.getMeasuredHeight(), view.getMeasuredWidth()};
+        else
+            return new int[]{view.getMeasuredWidth(), view.getMeasuredHeight()};
     }
 
     @Override
@@ -146,9 +176,9 @@ public class CameraFragment extends Fragment implements View.OnTouchListener, Ca
         int mHeight = mCamera.getParameters().getPreviewSize().height;
         frManager = new FRManager();
         frManager.init(mWidth, mHeight, resultRecorder);
-        frManager.setDelay(3000);
+        frManager.setDelay(1000);
+//        frManager.setFaceMatchListener(faceMatchListener);
         frManager.setOnFaceDetectedListener((afrFace, faceBitmap) -> {
-                    Log.i(TAG, "initFR: face detected!");
                     if (null != onFaceDetectedListener)
                         onFaceDetectedListener.onFaceDetected(afrFace, faceBitmap);
                 }
